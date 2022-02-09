ce90c66573fc681f8041a38e39cf7026a8f7abe4
Octavian-Leonard Chiriches <octavian.chiriches@endava.com>
2022-02-09 09:13:22 +0200

    chapter_seven_nullableTypes_miniExercises

---
+++ lib/tutorial/chapter_seven_nullable.dart
@@ -0,0 +1,25 @@
+void main() {
+
+  print("---------------------------");
+  print("Mini Exercises;");
+  print("---------------------------");
+
+  print('1. Create a String? variable called profession, but don’t give it a value. Then you’ll have profession null. :]');
+
+  // String? profession;
+  // print(profession);
+  print("---------------------------");
+
+  print('2. Give profession a value of “basketball player”.');
+
+  String? profession = 'basketball player';
+  print(profession);
+  print("---------------------------");
+
+  print('3. Write the following line and then hover your cursor over the variable name. What type does Dart infer iLove to be?');
+
+  const iLove = 'Dart';
+  print(iLove.runtimeType);
+  print("---------------------------");
+
+}
